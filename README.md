f83d0f0da96f56efe7ce0b3f72b7a15bfe4ffc14
marco <marco.kreft@iav.de>
2021-02-07 16:46:00 +0100

    add license and manifest file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ldf_parser
-Simple regex based parser for the lin discription file.
+Simple regex based parser for the lin discription file. It does what the title says and
+nothing more!
+
+# Developing ldf_parser
+To install ldf_parser, along with the tools
+you need to develop and run tests, run the
+following in your virtualenv:
+
+```bash
+$ pip install -e .[dev]
+```
--- setup.py
+++ setup.py
@@ -1,9 +1,32 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="lin_parser",
     version="0.01",
     description="Simple regex based parser for the lin description file.",
     py_modules="lin_parser",
     package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "numpy ~= 1.20.0",
+        "pandas ~= 1.2.1",
+        "pip ~= 21.0.1",
+    ],
+    extras_require={
+        "dev": [
+            "pytest >= 6.2.2",
+        ]
+    },
+    url="https://github.com/makreft/ldf_parser",
+    author="Marco Kreft",
 )
