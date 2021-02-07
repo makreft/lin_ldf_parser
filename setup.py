f9d979ce23c03482bcd2c2067b5c4372fea0cc3c
marco <marco.kreft@iav.de>
2021-02-07 15:29:43 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="lin_parser",
+    version="0.01",
+    description="Simple regex based parser for the lin description file.",
+    py_modules="lin_parser",
+    package_dir={"": "src"},
+)
